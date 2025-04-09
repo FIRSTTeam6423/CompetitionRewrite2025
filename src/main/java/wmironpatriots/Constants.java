16599cbecf23731b1448e3ea7055c94d6696561a
Dasun L. Abeykoon <Dasun20202020@hotmail.com>
2025-04-09 16:09:08 +0000

    Log PDH and scheduler
    Signed-off-by: Dasun L. Abeykoon <Dasun20202020@hotmail.com>

--- src/main/java/wmironpatriots/Constants.java
+++ src/main/java/wmironpatriots/Constants.java
@@ -1,9 +1,12 @@
 package wmironpatriots;
 
 import static edu.wpi.first.units.Units.Seconds;
+import static edu.wpi.first.units.Units.Volts;
 
 import edu.wpi.first.units.measure.Time;
+import edu.wpi.first.units.measure.Voltage;
 
 public class Constants {
-    public static final Time kLOOPTIME = Seconds.of(0.02);
+    public static final Time LOOPTIME = Seconds.of(0.02);
+    public static final Voltage BROWNOUT_VOLTAGE = Volts.of(6.0);
 }
--- src/main/java/wmironpatriots/Robot.java
+++ src/main/java/wmironpatriots/Robot.java
@@ -7,17 +7,24 @@
 package wmironpatriots;
 
 import static edu.wpi.first.units.Units.Seconds;
+import static edu.wpi.first.units.Units.Volts;
 
 import java.util.function.BiConsumer;
 
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.PowerDistribution;
+import edu.wpi.first.wpilibj.RobotController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import lib.LoggedCommandRobot;
 import monologue.Monologue;
 
 public class Robot extends LoggedCommandRobot {
+  private final PowerDistribution pdh = new PowerDistribution();
+
   public Robot() {
-    super(Constants.kLOOPTIME.in(Seconds));
+    super(Constants.LOOPTIME.in(Seconds));
     // logs build data to the datalog
     final String metadata = "/BuildData/";
     Monologue.log(metadata + "RuntimeType", getRuntimeType().toString());
@@ -37,7 +44,7 @@ public class Robot extends LoggedCommandRobot {
         Monologue.log(metadata + "GitDirty", "Unknown");
         break;
     }
-    
+
     BiConsumer<Command, Boolean> logCommandFunction =
         (Command command, Boolean active) -> {
           Monologue.log("Commands/" + command.getName(), active);
@@ -57,6 +64,19 @@ public class Robot extends LoggedCommandRobot {
     // ! Uncomment next line if you expirence massive lag/loop-overuns while connected to fms
     // SignalLogger.stop(); SignalLogger.enableAutoLogging(false);
 
+    SmartDashboard.putData(scheduler);
+    SmartDashboard.putData("PDH", pdh);
+
+    // This won't do anything unless we switch in a RIO 2.0
+    RobotController.setBrownoutVoltage(Constants.BROWNOUT_VOLTAGE.in(Volts));
+
+    if (isReal()) {
+      pdh.clearStickyFaults();
+      pdh.setSwitchableChannel(true);
+    } else {
+      DriverStation.silenceJoystickConnectionWarning(true);
+    }
+    
     configureGameBehavior();
     configureBindings();
   }
